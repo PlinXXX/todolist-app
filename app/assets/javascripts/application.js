6ee646648b359d8edf55982bca2abb8fbbc64e8a
santatramaximin <santatramaximin@gmail.com>
2019-03-01 08:38:12 +0100

    Sign Up not yet perfect and need some updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,53 @@ $(document).ready(function() {
     event.preventDefault(); 
     alert('You clicked the Hide link');	
   });
+// 2.1 Tabulations
+	function Tabulation() {
+		// let liListContainer = document.getElementsByClassName('container')[1];
+		// console.log(liListContainer);
+		// let cards = liListContainer.querySelectorAll('.card');
+		// console.log(cards);
 
-	$navButton = $('.navbar-toggler');
-	$navButton.on('click', () => {
-		$('#navbarTogglerDemo03').toggle('collapse');
-		console.log($('.nav-item'));
-	});
+		$list = $('.container li');
+		$cards = $('.card');
+
+			$list.eq(0).on('click', () => {
+				$cards.eq(0).toggle();
+			});
+
+			$list.eq(4).on('click', () => {
+				$cards.eq(1).toggle();
+			});			
+
+			$list.eq(8).on('click', () => {
+				$cards.eq(2).toggle();
+			});
+	}		
+	Tabulation();
+// 2.2 Le minimum vital du JavaScript de front
+
+ 	// 2.2.1 Le dropdown qui fait plaisir
+ 	function dropDown() {
+		$navButton = $('.navbar-toggler');
+		$navButton.on('click', () => {
+			$('#navbarTogglerDemo03').toggle('collapse');
+		});
+	}
+	dropDown();
+
+	// 2.2.2 Modales de connexion/inscription
+	function modales() {
+		$loginForm = $('#login');
+		console.log($loginForm);
+		$signupForm = $('#signup');
+		console.log($signupForm);
+		$signUpButton = $('.btn.btn-success').eq(0);
+		console.log($signUpButton);
+
+		$signUpButton.on('click', () => {
+			$signupForm.toggle('collapse');
+		})
+	}
+	modales();
+
 });	
